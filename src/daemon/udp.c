a5e77636f264cc04ef0b2bd5f65ae1609ecf1a18
Gu Jingxiao <10302010020@fudan.edu.cn>
2013-06-10 19:15:19 +0800

    create udp.c and move init_udp from socket.c to udp.c

---
+++ src/daemon/udp.c
@@ -0,0 +1,23 @@
+#include "udp.h"
+
+int init_udp_server_socket(int port){
+    int listenfd; 
+    struct sockaddr_in serveraddr;
+
+    if ((listenfd = socket( PF_INET, SOCK_DGRAM, 0 )) < 0)
+        unix_error("Creating UDP Socket Error");
+
+    /* Listenfd will be an endpoint for all requests to port
+       on any IP address for this host */
+    bzero((char *) &serveraddr, sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET; 
+    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY); 
+    serveraddr.sin_port = htons((unsigned short)port); 
+    
+    if (bind(listenfd, (struct sockaddr *) &serveraddr, sizeof(serveraddr))<0) 
+        unix_error("UDP Binding Error");
+
+    printf( "UDP on port %d\n", port);
+
+    return listenfd;
+}
---
+++ src/daemon/udp.h
@@ -0,0 +1,9 @@
+#ifndef __UDP_H__
+#define __UDP_H__
+
+#include "../common/csapp.h"
+#include "rtgrading.h"
+
+int init_udp_server_socket(int port);
+
+#endif /*__UDP_H__*/
